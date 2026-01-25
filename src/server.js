13357d2a8c77af87d0a9fabc41bc4a0c4f06b4d5
Bhanutejanallamothu <163739352+Bhanutejanallamothu@users.noreply.github.com>
2026-01-25 11:17:41 +0530

    DB update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 require("dotenv").config()
 const sequelize = require("./config/db")
 const app = require("./app")
+const cors = require("cors")
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://6000-firebase-studio-1765370272139.cluster-xpmcxs2fjnhg6xvn446ubtgpio.cloudworkstations.dev"
+    ],
+    credentials: true
+  })
+)
 
 sequelize.sync().then(() => {
   app.listen(process.env.PORT, () => {
-    console.log("Server running")
+    console.log(`Server running on port ${process.env.PORT}`)
   })
 })
