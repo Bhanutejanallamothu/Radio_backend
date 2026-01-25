978e6cdebdf88040c55be28619cbf57ed9a20162
Bhanutejanallamothu <163739352+Bhanutejanallamothu@users.noreply.github.com>
2026-01-25 21:59:56 +0530

    Update role.js

--- src/middleware/role.js
+++ src/middleware/role.js
@@ -1,19 +1,6 @@
-module.exports = (...allowedRoles) => {
-  return (req, res, next) => {
-    const role = req.user?.role
-
-    if (!role) {
-      return res.status(401).json({ message: "Unauthorized" })
-    }
-
-    if (role === "station_head") {
-      return next()
-    }
-
-    if (!allowedRoles.includes(role)) {
-      return res.status(403).json({ message: "Forbidden" })
-    }
-
-    next()
+module.exports = roles => (req, res, next) => {
+  if (!roles.includes(req.user.role)) {
+    return res.status(403).json({ message: "Forbidden" })
   }
+  next()
 }
