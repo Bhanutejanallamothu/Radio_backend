96d3385a24dfd6d9bb80f9101d65c03862d647bc
Bhanutejanallamothu <163739352+Bhanutejanallamothu@users.noreply.github.com>
2026-01-27 14:19:49 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,15 +7,15 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://6000-firebase-studio-1765370272139.cluster-xpmcxs2fjnhg6xvn446ubtgpio.cloudworkstations.dev",
-      "https://signup-guardian.vercel.app/"
+      "https://signup-guardian.vercel.app",
+      "https://6000-firebase-studio-1765370272139.cluster-xpmcxs2fjnhg6xvn446ubtgpio.cloudworkstations.dev/"
     ],
     credentials: true
   })
 )
 
-sequelize.sync().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
-  })
-})
+if (process.env.VERCEL !== "1") {
+  sequelize.sync()
+}
+
+module.exports = app
