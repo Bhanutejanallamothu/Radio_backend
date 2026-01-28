3c30cf1af7fdf2ebac3ae9134da6cdf6d1fc924b
Bhanutejanallamothu <163739352+Bhanutejanallamothu@users.noreply.github.com>
2026-01-28 15:28:28 +0530

    update

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
-require("dotenv").config()
-const sequelize = require("./config/db")
-const app = require("./app")
-const cors = require("cors")
 const dns = require("dns")
 dns.setDefaultResultOrder("ipv4first")
+
+require("dotenv").config()
+
+const cors = require("cors")
+const sequelize = require("./config/db")
+const app = require("./app")
 
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
       "https://6000-firebase-studio-1765370272139.cluster-xpmcxs2fjnhg6xvn446ubtgpio.cloudworkstations.dev",
-      "https://signup-guardian.vercel.app/",
+      "https://signup-guardian.vercel.app",
       "https://9000-firebase-studio-1765370272139.cluster-xpmcxs2fjnhg6xvn446ubtgpio.cloudworkstations.dev"
     ],
     credentials: true
